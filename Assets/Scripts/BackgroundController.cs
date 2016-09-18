6104fbe558899c73f96e9d0fb8817db29138ea42
theDawckta <lasaterz@gmail.com>
2016-09-18 05:15:04 -0700

    Rearanging folders, adding Zeke test scene in, Moving initial scene into production

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public GameObject[] Backgrounds;
+    public float BackgroundSpeedMultiplier;
+    private Material[] BackgroundMaterials;
+    private float[] BackgroundSpeeds;
+
+	void Start () 
+    {
+        BackgroundMaterials = new Material[Backgrounds.Length];
+        BackgroundSpeeds = new float[Backgrounds.Length];
+        for (int i = 0; i < Backgrounds.Length; i++)
+        {
+            BackgroundMaterials[i] = Backgrounds[i].GetComponent<Renderer>().material;
+            BackgroundSpeeds[i] = BackgroundSpeedMultiplier * (Backgrounds.Length - i);
+        }
+	}
+	
+	void Update () 
+    {
+        for (int i = 0; i < BackgroundMaterials.Length; i++ )
+        {
+            Vector2 offset = new Vector2(transform.position.x / Backgrounds[i].transform.localScale.x, transform.position.y / Backgrounds[i].transform.localScale.y);
+            BackgroundMaterials[i].mainTextureOffset = new Vector2(offset.x % 1, offset.y % 1);
+        }
+            
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Acceleration = 10.0f;
+    public float MaxSpeed = 10.0f;
+    public float RotateSpeed = 100.0f;
+    public float GunCoolDown = 1.0f;
+    public GameObject Nose;
+    public GameObject PlayerBullet;
+
+    private Rigidbody2D shipRigidbody;
+    private bool firing = false;
+
+    void Start()
+    {
+        shipRigidbody = transform.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float xVel = transform.InverseTransformDirection(shipRigidbody.velocity).x;
+        float yVel = transform.InverseTransformDirection(shipRigidbody.velocity).y;
+
+        if ((xVel + yVel) < MaxSpeed)
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                shipRigidbody.AddForce(transform.up * Acceleration);
+            }
+            else if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                shipRigidbody.AddForce(-transform.up * Acceleration);
+            }
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Rotate(Vector3.forward * (Time.deltaTime * RotateSpeed));
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Rotate(-Vector3.forward * (Time.deltaTime * RotateSpeed));
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Jump") && !firing)
+        {
+            StartCoroutine("FireGun");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    IEnumerator FireGun()
+    {
+        firing = true;
+        GameObject playerBullet = Instantiate<GameObject>(PlayerBullet, Nose.transform.position, Nose.transform.rotation);
+        playerBullet.transform.parent = transform.root;
+        
+        float timePassed = 0.0f;
+        while ((timePassed / GunCoolDown) <= 1)
+        {
+            timePassed = timePassed + Time.deltaTime;
+            yield return null;
+        }
+
+        firing = false;
+    }
+}
