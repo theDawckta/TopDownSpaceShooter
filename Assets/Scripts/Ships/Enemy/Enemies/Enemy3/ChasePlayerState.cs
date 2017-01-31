2c1bf83c9ddd43a5c229b8b623bb0e78b1381840
theDawckta <lasaterz@gmail.com>
2017-01-30 23:01:13 -0800

    adding states for Enemy3

---
+++ Assets/Scripts/Ships/Enemy/Enemies/Enemy3/ChasePlayerState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayerState : FSMState
+{
+    public ChasePlayerState()
+    {
+        stateID = StateID.ChasingPlayer;
+    }
+
+    public override void Reason(GameObject player, StarShip npc)
+    {
+        // If the player has gone 30 meters away from the NPC, fire LostPlayer transition
+        if (Vector3.Distance(npc.transform.position, player.transform.position) >= 30)
+            npc.GetComponent<Enemy3>().SetTransition(Transition.LostPlayer);
+    }
+
+    public override void Act(GameObject player, StarShip npc)
+    {
+        // Follow the path of waypoints
+        // Find the direction of the player 		
+        Vector3 vel = npc.GetComponent<Rigidbody2D>().velocity;
+        Vector3 moveDir = player.transform.position - npc.transform.position;
+
+        // Rotate towards the waypoint
+        npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
+                                                  Quaternion.LookRotation(moveDir),
+                                                  5 * Time.deltaTime);
+        npc.transform.eulerAngles = new Vector3(0, npc.transform.eulerAngles.y, 0);
+
+        vel = moveDir.normalized * 10;
+
+        // Apply the new Velocity
+        npc.GetComponent<Rigidbody2D>().velocity = vel;
+    }
+
+} // ChasePlayerState
---
+++ Assets/Scripts/Ships/Enemy/Enemies/Enemy3/FollowPathState.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathState : FSMState
+{
+    private int currentWayPoint;
+    private Transform[] waypoints;
+
+    public FollowPathState(Transform[] wp)
+    {
+        waypoints = wp;
+        currentWayPoint = 0;
+        stateID = StateID.FollowingPath;
+    }
+
+    public override void Reason(GameObject player, GameObject npc)
+    {
+        // If the Player passes less than 15 meters away in front of the NPC
+        //RaycastHit hit;
+        //if (Physics.Raycast(npc.transform.position, npc.transform.forward, out hit, 15F))
+        //{
+        //    if (hit.transform.gameObject.tag == "Player")
+        //        npc.GetComponent<Enemy3>().SetTransition(Transition.SawPlayer);
+        //}
+    }
+
+    public override void Act(GameObject player, StarShip npc)
+    {
+        // Follow the path of waypoints
+        // Find the direction of the current way point 
+        Vector3 vel = npc.GetComponent<Rigidbody2D>().velocity;
+        Vector3 moveDir = waypoints[currentWayPoint].position - npc.transform.position;
+
+        if (moveDir.magnitude < 1)
+        {
+            currentWayPoint++;
+            if (currentWayPoint >= waypoints.Length)
+            {
+                currentWayPoint = 0;
+            }
+            npc.MoveTarget(1.0f, waypoints[currentWayPoint].position);
+        }
+        else
+        {
+            vel = moveDir.normalized * 10;
+
+            // Rotate towards the waypoint
+            npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
+                                                      Quaternion.LookRotation(moveDir),
+                                                      5 * Time.deltaTime);
+            npc.transform.eulerAngles = new Vector3(0, npc.transform.eulerAngles.y, 0);
+
+        }
+
+        // Apply the Velocity
+        npc.GetComponent<Rigidbody2D>().velocity = vel;
+    }
+
+} // FollowPathState
