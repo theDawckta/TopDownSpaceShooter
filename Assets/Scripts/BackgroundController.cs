a0c1f0595f2488bfc9a53db30e8da74abdcb7bca
Zeke Lasater <zekelasater@Zekes-MacBook-Pro.local>
2019-05-16 18:51:51 -0700

    building main scenen back up

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -27,6 +27,8 @@ public class BackgroundController : MonoBehaviour
             Vector2 offset = new Vector2(Subject.transform.position.x / Backgrounds[i].transform.localScale.x, Subject.transform.position.y / Backgrounds[i].transform.localScale.y);
 			_backgroundMaterials[i].SetTextureOffset("_MainTex", new Vector2((offset.x) * BackgroundSpeeds[i], (offset.y) * BackgroundSpeeds[i]));
           	_backgroundMaterials[i].SetTextureOffset("_BumpMap", new Vector2((offset.x) * BackgroundSpeeds[i], (offset.y) * BackgroundSpeeds[i]));
+
+            Vector2 newOffset = _backgroundMaterials[i].GetTextureOffset("_MainTex");
         }   
     }
 }
