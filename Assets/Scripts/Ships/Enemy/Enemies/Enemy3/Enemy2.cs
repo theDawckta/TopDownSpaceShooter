d1d87cb8f1e810bb73751da052a904c4e71423c9
theDawckta <lasaterz@gmail.com>
2021-07-19 00:29:08 -0700

    Enemy2 setup, will use NavMeshAgent

---
+++ Assets/Scripts/Ships/Enemy/Enemies/Enemy3/Enemy2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy2 : MonoBehaviour
+{
+
+    private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
